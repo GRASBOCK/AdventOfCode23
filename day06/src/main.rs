96b7b40d6a894be427bf03ffab934751f0eed3df
Richard Körner <grasbock@gmail.com>
2024-03-28 19:55:27 +0700

    day 06 part 1

--- day06/src/main.rs
+++ day06/src/main.rs
@@ -9,9 +9,21 @@ fn parse_input(input: &str) -> Vec<(u64, u64)> {
     (0..durations.len()).map(|i| (durations[i], distances[i])).collect()
 }
 
+fn number_of_ways(race: &(u64, u64)) -> u64{
+    let p = - (race.0 as f64);
+    let q = race.1 as f64;
+    fn pq_formel(p: f64, q: f64) -> (f64, f64){
+        let a = -p/2.0;
+        let b = ((p/2.0).powi(2) - q).sqrt();
+        (a + b, a - b)
+    }
+    let (v_max, v_min) = pq_formel(p, q);
+    let (t_press_min, t_press_max) = ((v_min + 0.55).round() as u64, (v_max-0.55).round() as u64);
+    t_press_max - t_press_min + 1
+}
 
 fn solve_part1(races: &Vec<(u64, u64)>) -> u64{
-    0
+    races.iter().map(|r| number_of_ways(r)).fold(1, |acc, x| acc*x)
 }
 
 fn main() {
@@ -39,6 +51,16 @@ Distance:  9  40  200";
     fn test_parsing() {
         let races = example_races!();
         assert_eq!(parse_input(&EXAMPLE), races);
+    }
+
+    #[test]
+    fn test_nways() {
+        let races = example_races!();
+        let win_ways = vec![4, 8, 9];
+        for (i, r) in races.iter().enumerate(){
+            assert_eq!(number_of_ways(&r), win_ways[i]);
+        }
+        
     }
 
     #[test]
