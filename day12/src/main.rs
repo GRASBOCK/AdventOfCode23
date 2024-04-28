579990e4783f26a9aa789d9ed5491daa7327f7ce
Richard Körner <grasbock@gmail.com>
2024-04-29 00:21:30 +0700

    day12 p1 tests pass, but wrong answer

--- day12/src/main.rs
+++ day12/src/main.rs
@@ -1,7 +1,7 @@
 #[derive(PartialEq, Debug)]
-struct Row<'a>{
+struct Row<'a> {
     springs: &'a str,
-    groups: Vec<usize>, 
+    groups: Vec<usize>,
 }
 
 #[derive(PartialEq, Debug)]
@@ -9,13 +9,14 @@ struct PuzzleInput<'a> {
     rows: Vec<Row<'a>>,
 }
 
-fn parse_row(line: &str) -> Row{
+fn parse_row(line: &str) -> Row {
     let mut parts = line.split_whitespace();
     let springs = parts.next().unwrap();
     let records = parts.next().unwrap();
-    let groups = records.split(",").map(|s|{
-        s.parse::<usize>().unwrap()
-    }).collect();
+    let groups = records
+        .split(',')
+        .map(|s| s.parse::<usize>().unwrap())
+        .collect();
     Row { springs, groups }
 }
 
@@ -24,8 +25,72 @@ fn parse_input(input: &str) -> PuzzleInput {
     PuzzleInput { rows }
 }
 
+fn arrangements(springs: &[u8], groups: &[usize]) -> usize {
+    // gather matches
+    let n = groups[0];
+    let check_next = |i: usize| -> usize {
+        if groups.len() > 1 {
+            arrangements(&springs[i..], &groups[1..])
+        } else {
+            1
+        }
+    };
+    let mut arr = 0;
+    let pattern_matches = |start: usize| -> Option<usize> {
+        if start + n > springs.len() {
+            return None;
+        }
+        // there is still space for the group
+        let mut ok = true;
+        for j in 0..n {
+            if springs[start + j] == b'.' {
+                // the group finished
+                ok = false;
+                break;
+            }
+        }
+        if ok {
+            // the group can be full of '#'
+            if springs.len() > start + n {
+                // it's not over yet. There need to be a '.' after the group
+                if springs[start + n] != b'#' {
+                    // there is no '#' after the group
+                    return Some(start + n + 1);
+                }
+            } else {
+                return Some(start + n);
+            }
+        }
+        None
+    };
+    for (i, c) in springs.iter().enumerate() {
+        match c {
+            b'#' => {
+                if let Some(next_starting_index) = pattern_matches(i) {
+                    arr += check_next(next_starting_index);
+                }
+                break;
+            }
+            b'.' => {
+                continue;
+            }
+            b'?' => {
+                if let Some(next_starting_index) = pattern_matches(i) {
+                    arr += check_next(next_starting_index);
+                }
+            }
+            _ => panic!("invalid character {c}"),
+        }
+    }
+    arr
+}
+
 fn solve_part1(input: &PuzzleInput) -> usize {
-    0
+    input
+        .rows
+        .iter()
+        .map(|r| arrangements(r.springs.as_bytes(), &r.groups))
+        .sum()
 }
 
 fn solve_part2(input: &PuzzleInput) -> usize {
@@ -44,8 +109,7 @@ fn main() {
 mod tests {
     use super::*;
 
-    const EXAMPLE: &'static str = 
-"???.### 1,1,3
+    const EXAMPLE: &'static str = "???.### 1,1,3
 .??..??...?##. 1,1,3
 ?#?#?#?#?#?#?#? 1,3,1,6
 ????.#...#... 4,1,1
@@ -57,29 +121,29 @@ mod tests {
         () => {
             PuzzleInput {
                 rows: vec![
-                    Row{
+                    Row {
                         springs: "???.###",
-                        groups: vec![1,1,3]
+                        groups: vec![1, 1, 3],
                     },
-                    Row{
+                    Row {
                         springs: ".??..??...?##.",
-                        groups: vec![1,1,3]
+                        groups: vec![1, 1, 3],
                     },
-                    Row{
+                    Row {
                         springs: "?#?#?#?#?#?#?#?",
-                        groups: vec![1,3,1,6]
+                        groups: vec![1, 3, 1, 6],
                     },
-                    Row{
+                    Row {
                         springs: "????.#...#...",
-                        groups: vec![4,1,1]
+                        groups: vec![4, 1, 1],
                     },
-                    Row{
+                    Row {
                         springs: "????.######..#####.",
-                        groups: vec![1,6,5]
+                        groups: vec![1, 6, 5],
                     },
-                    Row{
+                    Row {
                         springs: "?###????????",
-                        groups: vec![3,2,1]
+                        groups: vec![3, 2, 1],
                     },
                 ],
             }
@@ -90,6 +154,18 @@ mod tests {
     fn test_parsing() {
         let input = example_parsed!();
         assert_eq!(parse_input(&EXAMPLE), input);
+    }
+
+    #[test]
+    fn test_arrangements() {
+        let input = example_parsed!();
+        let arr = |i: usize| arrangements(&input.rows[i].springs.as_bytes(), &input.rows[i].groups);
+        assert_eq!(arr(0), 1);
+        assert_eq!(arr(1), 4);
+        assert_eq!(arr(2), 1);
+        assert_eq!(arr(3), 1);
+        assert_eq!(arr(4), 4);
+        assert_eq!(arr(5), 10);
     }
 
     #[test]
