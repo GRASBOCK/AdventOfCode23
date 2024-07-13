550273999828919f7f648ba5ad4fdc297351c148
Richard Körner <grasbock@gmail.com>
2024-07-14 03:20:40 +0700

    fmt

--- day14/src/main.rs
+++ day14/src/main.rs
@@ -1,8 +1,10 @@
 use std::str;
 
 #[derive(PartialEq, Debug, Clone)]
-enum Tile{
-    O, C, E
+enum Tile {
+    O,
+    C,
+    E,
 }
 
 use Tile::*;
@@ -16,22 +18,26 @@ struct PuzzleInput {
 type Platform = PuzzleInput;
 
 fn parse_input(input: &str) -> PuzzleInput {
-    let tiles = input.as_bytes().iter().filter_map(|b| match b{
-        b'O' => Some(O),
-        b'#' => Some(C),
-        b'.' => Some(E),
-        b'\n' => None,
-        _ => panic!("invalid character {}", *b as char)
-    }).collect();
+    let tiles = input
+        .as_bytes()
+        .iter()
+        .filter_map(|b| match b {
+            b'O' => Some(O),
+            b'#' => Some(C),
+            b'.' => Some(E),
+            b'\n' => None,
+            _ => panic!("invalid character {}", *b as char),
+        })
+        .collect();
     let width = input.find("\n").expect("no new line found");
-    PuzzleInput { tiles, width}
+    PuzzleInput { tiles, width }
 }
 
-fn tilt_platform(platform: &Platform) -> Platform{
+fn tilt_platform(platform: &Platform) -> Platform {
     platform.clone()
 }
 
-fn platform_load(platform: &Platform) -> usize{
+fn platform_load(platform: &Platform) -> usize {
     0
 }
 
@@ -50,7 +56,6 @@ fn main() {
     println!("Part 1: {}", solve_part1(&platform));
     println!("Part 2: {}", solve_part2(&platform));
 }
-
 
 #[cfg(test)]
 mod tests {
@@ -72,17 +77,12 @@ O.#..O.#.#
         () => {
             PuzzleInput {
                 tiles: vec![
-                O,E,E,E,E,C,E,E,E,E,
-                O,E,O,O,C,E,E,E,E,C,
-                E,E,E,E,E,C,C,E,E,E,
-                O,O,E,C,O,E,E,E,E,O,
-                E,O,E,E,E,E,E,O,C,E,
-                O,E,C,E,E,O,E,C,E,C,
-                E,E,O,E,E,C,O,E,E,O,
-                E,E,E,E,E,E,E,O,E,E,
-                C,E,E,E,E,C,C,C,E,E,
-                C,O,O,E,E,C,E,E,E,E,],
-                width: 10
+                    O, E, E, E, E, C, E, E, E, E, O, E, O, O, C, E, E, E, E, C, E, E, E, E, E, C,
+                    C, E, E, E, O, O, E, C, O, E, E, E, E, O, E, O, E, E, E, E, E, O, C, E, O, E,
+                    C, E, E, O, E, C, E, C, E, E, O, E, E, C, O, E, E, O, E, E, E, E, E, E, E, O,
+                    E, E, C, E, E, E, E, C, C, C, E, E, C, O, O, E, E, C, E, E, E, E,
+                ],
+                width: 10,
             }
         };
     }
@@ -98,17 +98,12 @@ O.#..O.#.#
         let platform = example_parsed!();
         let platform_tilted = Platform {
             tiles: vec![
-                O,O,O,O,E,C,E,O,E,E,
-                O,O,E,E,C,E,E,E,E,C,
-                O,O,E,E,O,C,C,E,E,O,
-                O,E,E,C,E,O,O,E,E,E,
-                E,E,E,E,E,E,E,E,C,E,
-                E,E,C,E,E,E,E,C,E,C,
-                E,E,O,E,E,C,E,O,E,O,
-                E,E,O,E,E,E,E,E,E,E,
-                C,E,E,E,E,C,C,C,E,E,
-                C,E,E,E,E,C,E,E,E,E,],
-            width: 10
+                O, O, O, O, E, C, E, O, E, E, O, O, E, E, C, E, E, E, E, C, O, O, E, E, O, C, C, E,
+                E, O, O, E, E, C, E, O, O, E, E, E, E, E, E, E, E, E, E, E, C, E, E, E, C, E, E, E,
+                E, C, E, C, E, E, O, E, E, C, E, O, E, O, E, E, O, E, E, E, E, E, E, E, C, E, E, E,
+                E, C, C, C, E, E, C, E, E, E, E, C, E, E, E, E,
+            ],
+            width: 10,
         };
         assert_eq!(tilt_platform(&platform), platform_tilted);
     }
@@ -118,7 +113,6 @@ O.#..O.#.#
         let input = example_parsed!();
         assert_eq!(solve_part1(&input), 136);
     }
-
 
     #[test]
     fn test_solve_part2() {
