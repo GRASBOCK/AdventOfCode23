dcbe04dbcbd2b040a92f26eea2dfee2732125b40
Richard Körner <grasbock@gmail.com>
2024-09-01 19:53:35 +0700

    day 17 part 1 WIP

--- day17/src/main.rs
+++ day17/src/main.rs
@@ -58,9 +58,82 @@ fn parse_input(input: &str) -> PuzzleInput {
     }
 }
 
+#[derive(Clone, Copy)]
+struct Path{
+    heatloss_since_origin: usize,
+    from_dir: (i32, i32)
+}
+
+const UP: (i32, i32) = (0, -1);
+const DOWN: (i32, i32) = (0, 1);
+const LEFT: (i32, i32) = (-1, 0);
+const RIGHT: (i32, i32) = (1, 0);
+
+impl fmt::Display for Path {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        if self.from_dir == UP{
+            write!(f, "v")?;
+        }else if self.from_dir == DOWN{
+            write!(f, "^")?;
+        }else if self.from_dir == LEFT{
+            write!(f, ">")?;
+        }else if self.from_dir == RIGHT{
+            write!(f, "<")?;
+        }else{
+            write!(f, ".")?;
+        }
+        Ok(())
+    }
+}
+
+fn determine_distance(xy: &(i32, i32), consecutive: u8, blocks: &Grid<u8>, paths: &mut Grid<Path>){
+    let directions = [UP, DOWN, LEFT, RIGHT];
+    let index = blocks.coord2index(*xy).unwrap();
+    let p = paths.tiles[index];
+    for dir in directions{
+        if p.from_dir == dir{
+            // don't check origin
+            continue;
+        }
+        let mut consecutive = consecutive;
+        if (-p.from_dir.0, -p.from_dir.1) != dir{
+            // not same direction
+            consecutive = 0;
+        }else if consecutive >= 3{
+            // don't allow more than 3 in the same direction
+            continue;
+        }
+        let xy_next = (xy.0 + dir.0, xy.1 + dir.1);
+        let next_i = blocks.coord2index(xy_next);
+        if let Some(next_i) = next_i{
+            let b_next = blocks.tiles[next_i];
+            let p_next = &mut paths.tiles[next_i];
+            let go_to_next_heatloss = p.heatloss_since_origin + b_next as usize;
+            if p_next.heatloss_since_origin > go_to_next_heatloss{
+                p_next.heatloss_since_origin = go_to_next_heatloss;
+                p_next.from_dir = (-dir.0, -dir.1);
+                determine_distance(&xy_next, consecutive+1, blocks, paths);
+            }
+        }
+    }
+}
+
+fn total_heatloss_from_to(origin: &(i32, i32), target: &(i32, i32), blocks: &Grid<u8>) -> usize{
+    let path_tiles: Box<[Path]> = vec![Path{heatloss_since_origin: usize::MAX, from_dir: (0, 0)}; blocks.width*blocks.height].iter().copied().collect();
+    let mut paths = Grid::<Path>{tiles: path_tiles, width:blocks.width, height: blocks.height};
+    
+    let origin_i = blocks.coord2index(*origin).unwrap();
+    paths.tiles[origin_i].heatloss_since_origin = 0;
+    determine_distance(origin, 0, blocks, &mut paths);
+    
+    println!("{}", paths);
+
+    let target_i = blocks.coord2index(*target).unwrap();    
+    paths.tiles[target_i].heatloss_since_origin
+}
 
 fn solve_part1(input: &PuzzleInput) -> usize {
-    0
+    total_heatloss_from_to(&(0, 0), &(input.width as i32-1, input.height as i32-1), input)
 }
 
 fn solve_part2(input: &PuzzleInput) -> usize {
@@ -68,6 +141,7 @@ fn solve_part2(input: &PuzzleInput) -> usize {
 }
 
 fn main() {
+    // run in release mode, because otherwise recursion causes stack overflow
     let input = include_str!("../input");
     let inp = parse_input(input);
 
@@ -127,7 +201,7 @@ mod tests {
     #[test]
     fn test_solve_part1() {
         let input = example_parsed!();
-        assert_eq!(solve_part1(&input), 46);
+        assert_eq!(solve_part1(&input), 102);
     }
 
     #[test]
